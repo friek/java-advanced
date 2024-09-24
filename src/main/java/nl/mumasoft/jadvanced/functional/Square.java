675519cb9c3ae86373ac2edc68a6749fbb272c4f
Johan Mulder <johan@mumasoft.nl>
2024-09-24 14:08:05 +0200

    Added comment

--- src/main/java/nl/mumasoft/jadvanced/functional/Square.java
+++ src/main/java/nl/mumasoft/jadvanced/functional/Square.java
@@ -4,6 +4,7 @@ import java.util.function.Function;
 
 /**
  * Functional interface to square an integer
+ * <p>Basically the same as IntUnaryOperator</p>
  */
 @FunctionalInterface
 public interface Square extends Function<Integer, Integer>
