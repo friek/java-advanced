798cb96b2e06ae062bc026dc3e79f39000f74db2
Johan Mulder <johan@mumasoft.nl>
2024-09-24 15:52:53 +0200

    Removed list

--- src/main/java/nl/mumasoft/jadvanced/factory/Shapes.java
+++ src/main/java/nl/mumasoft/jadvanced/factory/Shapes.java
@@ -4,19 +4,21 @@ import nl.mumasoft.jadvanced.factory.model.Shape;
 import nl.mumasoft.jadvanced.factory.model.ShapeFactory;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Shapes
 {
 	public static void main(String[] args)
 	{
-		List<Shape> shapes = List.of(
-				ShapeFactory.createCircle(5),
-				ShapeFactory.createSquare(5),
-				ShapeFactory.createTriangle(4, 5),
-				ShapeFactory.createRectangle(4, 7)
-		);
+		double totalArea = Stream.of(
+						ShapeFactory.createCircle(5),
+						ShapeFactory.createSquare(5),
+						ShapeFactory.createTriangle(4, 5),
+						ShapeFactory.createRectangle(4, 7)
+				)
+				.mapToDouble(Shape::area)
+				.sum();
 
-		double totalArea = shapes.stream().mapToDouble(Shape::area).sum();
 		System.out.println("Total area: " + totalArea);
 	}
 }
